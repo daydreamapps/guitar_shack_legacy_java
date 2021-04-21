b00a1d50399901eeb7c8b5d4dbbfd5a83a3c46cf
Samuel Patmore <daydreamapplications@gmail.com>
2021-04-21 10:25:11 +0100

    move salesTotal to SalesHistory

--- src/main/java/com/guitarshack/SalesHistory.java
+++ src/main/java/com/guitarshack/SalesHistory.java
@@ -1,5 +1,13 @@
 package com.guitarshack;
 
+import com.google.gson.Gson;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SalesHistory {
     private final HttpService httpService;
 
@@ -9,5 +17,24 @@ public class SalesHistory {
 
     public HttpService getHttpService() {
         return httpService;
+    }
+
+    SalesTotal getSalesTotal(Product product, Date startDate, Date endDate) {
+        DateFormat format = new SimpleDateFormat("M/d/yyyy");
+
+        Map<String, Object> params1 = new HashMap<>(){{
+            put("productId", product.getId());
+            put("startDate", format.format(startDate));
+            put("endDate", format.format(endDate));
+            put("action", "total");
+        }};
+        String paramString1 = "?";
+
+        for (String key : params1.keySet()) {
+            paramString1 += key + "=" + params1.get(key).toString() + "&";
+        }
+        String result1 = getHttpService().fetchResponse("https://gjtvhjg8e9.execute-api.us-east-2.amazonaws.com/default/sales", paramString1);
+        SalesTotal total = new Gson().fromJson(result1, SalesTotal.class);
+        return total;
     }
 }
--- src/main/java/com/guitarshack/StockMonitor.java
+++ src/main/java/com/guitarshack/StockMonitor.java
@@ -2,8 +2,6 @@ package com.guitarshack;
 
 import com.google.gson.Gson;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -40,28 +38,9 @@ public class StockMonitor {
         calendar.add(Calendar.DATE, -30);
 
         Date startDate = calendar.getTime();
-        SalesTotal total = getSalesTotal(product, startDate, endDate, new SalesHistory(httpService));
+        SalesTotal total = new SalesHistory(httpService).getSalesTotal(product, startDate, endDate);
         if(product.getStock() - quantity <= (int) ((double) (total.getTotal() / 30) * product.getLeadTime()))
             alert.send(product);
-    }
-
-    private SalesTotal getSalesTotal(Product product, Date startDate, Date endDate, SalesHistory salesHistory) {
-        DateFormat format = new SimpleDateFormat("M/d/yyyy");
-
-        Map<String, Object> params1 = new HashMap<>(){{
-            put("productId", product.getId());
-            put("startDate", format.format(startDate));
-            put("endDate", format.format(endDate));
-            put("action", "total");
-        }};
-        String paramString1 = "?";
-
-        for (String key : params1.keySet()) {
-            paramString1 += key + "=" + params1.get(key).toString() + "&";
-        }
-        String result1 = salesHistory.getHttpService().fetchResponse("https://gjtvhjg8e9.execute-api.us-east-2.amazonaws.com/default/sales", paramString1);
-        SalesTotal total = new Gson().fromJson(result1, SalesTotal.class);
-        return total;
     }
 
     private String fetchResponse(String baseUrl, String query) {
