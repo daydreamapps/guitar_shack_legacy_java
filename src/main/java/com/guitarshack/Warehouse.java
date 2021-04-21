92f04b07d06e130ad8019fe1b455061e6decf7bc
Samuel Patmore <daydreamapplications@gmail.com>
2021-04-21 11:10:26 +0100

    Cleanup Warehouse

--- src/main/java/com/guitarshack/Warehouse.java
+++ src/main/java/com/guitarshack/Warehouse.java
@@ -12,10 +12,6 @@ public class Warehouse {
         this.httpService = httpService;
     }
 
-    public HttpService getHttpService() {
-        return httpService;
-    }
-
     Product getProduct(int productId) {
         String baseURL = "https://6hr1390c1j.execute-api.us-east-2.amazonaws.com/default/product";
         Map<String, Object> params = new HashMap<>() {{
@@ -26,8 +22,8 @@ public class Warehouse {
         for (String key : params.keySet()) {
             paramString += key + "=" + params.get(key).toString() + "&";
         }
-        String result = getHttpService().fetchResponse(baseURL, paramString);
-        Product product = new Gson().fromJson(result, Product.class);
-        return product;
+        String result = httpService.fetchResponse(baseURL, paramString);
+
+        return new Gson().fromJson(result, Product.class);
     }
 }
